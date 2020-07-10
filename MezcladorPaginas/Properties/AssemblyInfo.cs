16200c6648c35cc4b3937bb9333086a76ce9fe68
Miguel Ángel Cabrero <miguelangelcabrero@gmail.com>
2020-07-10 12:39:33 +0200

    Add files via upload

---
+++ MezcladorPaginas/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Mixer Pages")]
+[assembly: AssemblyDescription("Starting from a scanned document with odd and even pages, it sorts them into a new document.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("apps4every")]
+[assembly: AssemblyProduct("Mixer Pages")]
+[assembly: AssemblyCopyright("Copyright ©  2020 apps4every.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(true)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("7d44edae-51d4-4338-88ec-9f4b7daa77e3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
